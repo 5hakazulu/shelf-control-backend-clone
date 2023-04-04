5fc347e813cac7cb1794f0ae50955ff999916bba
Joshua Zuluaga <zazuofcloud10@gmail.com>
2023-04-04 18:16:02 -0400

    made some changes to get the server up and running.

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ const express = require('express')
 const app = express
 
 const { Sequalize } = require('sequalize')
-const sequalize = new Sequalize("postgres://shelfcontrol_user:a75s3hb6Jh7CuBZ3x6BFYcjH06QhgIte@dpg-cghodp2k728s1bpn159g-a/shelfcontrol")
+const sequalize = new Sequalize("postgres://shelfcontrol_user:postgres://shelf_control_backend_user:qcAjIHkZO8S0NDSYlGM4VOHCnrud3X2W@dpg-cgm9u9jhp8udl0s6nibg-a/shelf_control_backend")
